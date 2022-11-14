2a4ec2fa5a15ea6055e2b9703d8b42685751ba41
Jordan <jordslaptop@Jordans-MacBook-Pro.local>
2022-11-14 15:29:09 -0800

    fixed routes

--- api-server/1-server.js
+++ api-server/1-server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
 
-const config = require('./config.js')[process.env.NODE_ENV||"dev"];
+const config = require('./config')[process.env.NODE_ENV||"dev"];
 const port = config.port;
 
 const pool = new Pool ({connectionString: config.connectionString});
@@ -16,6 +16,7 @@ app.use(express.json());
 app.get(`/`, (req,res) =>{
     res.send("Jream-Garage-API");
 })
+
 //Cars
 app.get(`/api/cars`, (req,res) =>{
     pool.query(`SELECT * FROM Vehicle`)
@@ -40,18 +41,24 @@ app.get(`/api/companies`, (req,res) =>{
             res.send(404, "Not Found")
         })
 })
-//Makes
+
+// ===== GET REQUESTS =====
 app.get('/api/cars/:carsSearch', (req, res) => {
-    let makeRequest = req.params.carsSearch;
-    console.log("before slice", makeRequest)
-    let makeQuery = makeRequest.charAt(0).toUpperCase() + makeRequest.slice(1);
-    console.log(makeQuery);
-    console.log(req.params);
+    
+    let makeRequest = req.params.carsSearch;                                                           //User Input
+    let makeQuery = makeRequest.charAt(0).toUpperCase() + makeRequest.slice(1);                        //Capitilize First Letter
+
 
         pool.query(`SELECT * FROM vehicle WHERE
         make='${makeQuery}' OR model='${makeQuery}' OR type='${makeQuery}' OR color='${makeQuery}';`)
         .then(result => {
-            res.status(200).send(result.rows)
+
+            if (result.rows.length === 0){
+
+                res.status(404).send("Not Found")
+            } else {
+                res.status(200).send(result.rows)
+            }
         })
         .catch(e => {
             console.error(e.stack);
@@ -60,21 +67,101 @@ app.get('/api/cars/:carsSearch', (req, res) => {
   
 
 })
-//Models
-// app.get('/api/cars/:model', (req, res) => {
-//     let makeRequest = req.params.model;
-//     let makeQuery = makeRequest.charAt(0).toUpperCase() + makeRequest.slice(1);
-
-//     pool.query(`SELECT * FROM vehicle WHERE model='${makeQuery}';`)
-//     .then(result => {
-//         res.send(result.rows)
-//     })
-// })
+app.get('/api/companies/:compSearch', (req, res) => {
+    
+    let makeRequest = req.params.compSearch;                                                           //User Input
+    let makeQuery = makeRequest.charAt(0).toUpperCase() + makeRequest.slice(1);                        //Capitilize First Letter
 
 
+        pool.query(`SELECT * FROM companies WHERE
+        name='${makeQuery}' OR founder='${makeQuery}';`)
+        .then(result => {
+            
+            if (result.rows.length === 0){
+                res.status(404).send("Not Found")
+            } else {
+                
+                res.status(200).send(result.rows)
+            }
+        })
+        .catch(e => {
+            console.error(e.stack);
+            res.status(404).send("Not Found")
+        })
+  
+
+})
 
 
+// ===== POST REQUESTS =====
+app.post('/api/cars/', (req, res) =>{
+    pool.query(`INSERT INTO vehicle (make, model, year, type, color) VALUES ('${req.body.make}','${req.body.model}',${req.body.year},${req.body.type},${req.body.color})`)
+    .then((result) => {
+        console.log("Car Added")
+        res.status(200).send(result.rows);
+    })
+    .catch((e) => console.log(e.stack));
+})
+app.post('/api/cars/', (req,res) => {
+    pool.query(`INSERT INTO vehicle (make, model, year, type, color) VALUES ('${req.body.name}','${req.body.founder}',${req.body.date})`)
+    .then((result) => {
+        console.log("Company Added")
+        res.status(200).send(result.rows);
+    })
+    .catch((e) => console.log(e.stack));
+})
 
+// ===== PATCH REQUEST =====
+app.patch('/api/cars/:id', (req, res)=>{
+    var patchRequest = "SET";
+
+    pool.query(`SELECT * FROM vehicle WHERE id=${req.params.id}`)
+    .then((result) => {
+
+        var checkedKey = []; //CHECKS TABLE FIELDS
+        for(let i = 0; i < result.fields.length; i++){
+            checkedKey.push(result.fields[i].name);    
+        }
+        // CHECKS JSON BODY WITH FIELDS
+        for(ele in req.body) {
+            console.log(req.body)
+            let key = ele;
+            let value = req.body[key];
+                if(checkedKey.includes(key) === false){
+                    res.writeHead(400, {'Content-Type': 'text/plain'});
+                    return res.end(`Bad Request ${key} is invalid`);
+                } else if(typeof req.body[key] === 'number'){
+                    patchRequest += ` ${key}=${value},`;
+                } else {
+                    patchRequest += ` ${key}='${value}',`;
+                }
+            
+        }
+            let finalQuery = patchRequest.slice(0,-1);
+        
+                pool.query(`UPDATE vehicle ${finalQuery} WHERE id=${req.params.id}`)
+                .then(() => {
+                    res.writeHead(200, {'Content-Type': 'text/plain'})
+                    res.end(`You've successfully ${finalQuery}`);
+                })
+                .catch((e) => console.log(e.stack));
+
+    })
+    .catch((e) => console.error(e.stack));
+
+})
+//DONT TOUCH
+app.delete('/api/cars/:id', (req, res) => {
+
+    console.log(req.params);
+    pool.query(`DELETE FROM cars WHERE id=${id}`)
+    .then(
+        res.status(200).send("Deleted")
+    )
+    .catch(
+        res.status(404).send("Id not found")
+    )
+})
 
 app.listen(port, () => {                   
     console.log(`
