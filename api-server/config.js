c01471b1382e0176f0c6e26cff8428674d5b0add
Jordan <jordslaptop@Jordans-MacBook-Pro.local>
2022-11-15 13:40:24 -0800

    change

--- api-server/config.js
+++ api-server/config.js
@@ -5,7 +5,7 @@ module.exports = {
   },
 
   production: {
-    connectionString: process.env.POSTGRES_CONNECTION_STRING + "?ss1=true",
+    connectionString: process.env.POSTGRES_CONNECTION_STRING + "?ssl=true",
     port: process.env.PORT,
   },
 };
